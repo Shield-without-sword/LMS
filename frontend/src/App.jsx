0ecaf394b7f8c42d858bb53fff5e88028a190f77
Malhar2400 <120003057+Malhar2400@users.noreply.github.com>
2025-03-08 00:50:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,14 +10,13 @@ import Signup from './pages/Signup';
 import Home from './pages/Home';
 import LandingPage from './pages/LandingPage';
 import RefrshHandler from './RefrshHandler';
-
-import Community from './pages/Community';
-import Projects from './pages/Projects'; // Import Projects Page
+import RoadmapPage from './pages/Projects'; // Import Projects Page
 import Hackathon from './pages/Hackathon';
 import Courses from './pages/Courses'; // Import Courses Page
 import Mentorship from './pages/Mentorship'; // Import Mentorship Page
 import AvatarQA from './components/AvatarQA';
-
+import FrameVRRoom from './pages/FrameVRRoom';
+import CodePlayground from './pages/CodePlayground';
 import EmployeeManagementApp from './Elements/EmployeeManagementApp';
 import EmployeeDetails from './Elements/EmployeeDetails';
 
@@ -41,10 +40,13 @@ function App() {
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/framevr" element={<PrivateRoute><FrameVRRoom /></PrivateRoute>} />
+        <Route path="/codeplayground" element={<PrivateRoute><CodePlayground /></PrivateRoute>} />
+        <Route path="/logout" element={<Navigate to="/login" />} />
         <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
         <Route path="/interview" element={<PrivateRoute><AvatarQA /></PrivateRoute>} />
         <Route path="/community" element={<PrivateRoute><EmployeeManagementApp /></PrivateRoute>} />
-        <Route path="/projects" element={<PrivateRoute><Projects /></PrivateRoute>} />
+        <Route path="/projects" element={<PrivateRoute><RoadmapPage /></PrivateRoute>} />
         <Route path="/hackathon" element={<PrivateRoute><Hackathon /></PrivateRoute>} />
         <Route path="/courses/*" element={<Courses />} />
         <Route path="/mentorship" element={<PrivateRoute><Mentorship /></PrivateRoute>} />
