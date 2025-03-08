87f469531e33c40bde101dce362c960d5bd50856
Malhar2400 <120003057+Malhar2400@users.noreply.github.com>
2025-03-08 11:43:05 +0530

    Update EmployeeModel.js

--- backend/Models/EmployeeModel.js
+++ backend/Models/EmployeeModel.js
@@ -4,36 +4,31 @@ const Schema = mongoose.Schema;
 const EmployeeSchema = new Schema({
     name: {
         type: String,
-        required: true
     },
     Defect: {
         type: String,
-        required: true
     },
     Weight: {
         type: String,
-        required: true
     },
     Breed: {
         type: String,
-        required: true
     },
     profileImage: {
         type: String
     },
     Age: {
         type: Number,
-        required: true
     },
     production: [
         {
             date: {
                 type: Date,
-                required: true
+     
             },
             amount: {
                 type: Number,
-                required: true
+          
             }
         }
     ],
